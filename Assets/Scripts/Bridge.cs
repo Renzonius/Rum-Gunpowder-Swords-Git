daebeb81a5bea3788153acc695d927e37e70c674
Renzonius <48992705+Renzonius@users.noreply.github.com>
2025-05-11 15:16:59 -0300

    Level1 (Bridge Element)
    - The "GameManager" script was created.
    - The Bridge asset was created.
    - The "Bridge" script was created. This script contains the logic for the bridge asset.
    - The "BridgeController" script was created. This script controls all bridge assets. It is used by the "GameManager" script as a tool to activate/deactivate bridges.

---
+++ Assets/Scripts/Bridge.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class Bridge : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer bridgeSprite;
+
+    private void Awake()
+    {
+        bridgeSprite = GetComponent<SpriteRenderer>();
+    }
+
+    //Metodo que utilizamos para activar el puente desde el script "BridgeController".
+    public void ActivateBridge(float targetSize, float duration)
+    {
+        ChangeSize(targetSize, duration);
+    }
+    private async void ChangeSize(float targetSize, float duration)
+    {
+        await ChangeSizeAsync(targetSize, duration);
+    }
+
+    private async Task ChangeSizeAsync(float targetSize, float duration)
+    {
+        Vector3 initialScale = bridgeSprite.size;
+        Vector3 targetScale = new Vector3(targetSize, initialScale.y, initialScale.z);
+
+        float elapsedTime = 0f;
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = elapsedTime / duration;
+            bridgeSprite.size = Vector3.Lerp(initialScale, targetScale, t);
+            await Task.Yield();
+        }
+
+        // Asegurarse de que el tamaño final sea exactamente el objetivo.
+        bridgeSprite.size = targetScale;
+    }
+}
---
+++ Assets/Scripts/BridgeController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class BridgeController : MonoBehaviour
+{
+    [SerializeField] private Bridge[] bridges;
+    [SerializeField] private float tileMaxSize = 14f;
+    [SerializeField] private float tileMinSize = 0f;
+
+    //Metodo que tulizamos para ACTIVAR el puente desde el script "GameManager".
+    public void ExtendBridge(float duration, int count)
+    {
+         ChangeBridgeSize(tileMaxSize, duration, count);
+    }
+    //Metodo que tulizamos para DESACTIVAR el puente desde el script "GameManager".
+    public void CollapseBridge(float duration, int count)
+    {
+        ChangeBridgeSize(tileMinSize, duration, count);
+    }
+
+    private void ChangeBridgeSize(float targetSize, float duration, int count)
+    {
+        // Asegurarse de que el tamaño objetivo esté dentro de los límites.
+        targetSize = Mathf.Clamp(targetSize, tileMinSize, tileMaxSize);
+
+        // Limitar la cantidad de elementos a modificar al tamaño del arreglo.
+        count = Mathf.Clamp(count, 1, bridges.Length);
+
+        switch (count)
+        {
+            case 1:
+                bridges[0].ActivateBridge(targetSize, duration);
+                break;
+            case 2:
+                bridges[1].ActivateBridge(targetSize, duration);
+                bridges[2].ActivateBridge(targetSize, duration);
+                break;
+            case 3:
+                bridges[0].ActivateBridge(targetSize, duration);
+                bridges[1].ActivateBridge(targetSize, duration);
+                bridges[2].ActivateBridge(targetSize, duration);
+                break;
+            default:
+                Debug.LogWarning("El número de puentes a activar no es válido.");
+            break;
+        }
+    }
+}
+
